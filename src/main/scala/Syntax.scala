3c3dbcaa57340fcfdd72df7b46dbe80f3083e193
yelun <ye.yang20@imperial.ac.uk>
2022-02-03 02:26:29 +0000

    fix: bracket mismatch on character

--- src/main/scala/Syntax.scala
+++ src/main/scala/Syntax.scala
@@ -2,12 +2,12 @@ import parsley.Parsley
 import parsley.Parsley._
 
 object utility {
-	val keywords = Set(
+  val keywords = Set(
     "begin",
-	"end",
-	"is",
-	"skip",
-	"read",
+    "end",
+    "is",
+    "skip",
+    "read",
     "free",
     "return",
     "exit",
@@ -34,76 +34,80 @@ object utility {
     "chr",
     "true",
     "false",
-    "null")
+    "null"
+  )
 }
 
 /* Lexer */
 object lexer {
-	import parsley.character._
-	import parsley.token.{LanguageDef, Lexer, Parser, Predicate}
-    import parsley.implicits.character.{charLift, stringLift}
-    import parsley.combinator.{eof, many, manyUntil, optional, some}
-	val lex = new Lexer(LanguageDef.plain.copy(
-		commentStart = "#",
-        commentEnd = "\n",
-		space = Predicate(isWhitespace),
-		identStart = Parser("_" <|> letter),
-		identLetter = Parser("_" <|> alphaNum),
-		keywords = utility.keywords
-	))
+  import parsley.character._
+  import parsley.token.{LanguageDef, Lexer, Parser, Predicate}
+  import parsley.implicits.character.{charLift, stringLift}
+  import parsley.combinator.{eof, many, manyUntil, optional, some}
+  val lex = new Lexer(
+    LanguageDef.plain.copy(
+      commentStart = "#",
+      commentEnd = "\n",
+      space = Predicate(isWhitespace),
+      identStart = Parser("_" <|> letter),
+      identLetter = Parser("_" <|> alphaNum),
+      keywords = utility.keywords
+    )
+  )
 
-	def fully[A](p: => Parsley[A]): Parsley[A] = lex.whiteSpace ~> p <~ eof
+  def fully[A](p: => Parsley[A]): Parsley[A] = lex.whiteSpace ~> p <~ eof
 }
 
 /* Syntax Parser */
 object syntax {
-	import lexer.{fully, lex}
-    import parsley.implicits.character.{charLift, stringLift}
-    import parsley.character.{anyChar, digit, endOfLine, noneOf}
-    import parsley.combinator.{eof, many, manyUntil, optional, some}
+  import lexer.{fully, lex}
+  import parsley.implicits.character.{charLift, stringLift}
+  import parsley.character.{anyChar, digit, endOfLine, noneOf}
+  import parsley.combinator.{eof, many, manyUntil, optional, some}
 
-    /* COMMENTS */
+  /* COMMENTS */
 
-    // comment := '#' (any character except EOL)* <EOL>
-    // val comment = "#" ~> manyUntil(anyChar, endOfLine)
+  // comment := '#' (any character except EOL)* <EOL>
+  // val comment = "#" ~> manyUntil(anyChar, endOfLine)
 
+  /* LITERALS */
 
-    /* LITERALS */
+  // int-sign := '+' | '-'
+  val intSign = "+" <|> "-"
 
-    // int-sign := '+' | '-'
-    val intSign = "+" <|> "-"
+  // int-liter := <int-sign>? <digit>+
+  val intLiter = optional(intSign) <~> some(digit)
 
-    // int-liter := <int-sign>? <digit>+
-    val intLiter = optional(intSign) <~> some(digit)
+  // TODO: Try to use implicits to not use 'lex.keyword' everytime
+  // bool-liter := true | false
+  val boolLiter = lex.keyword("true") <|> lex.keyword("false")
 
-    // TODO: Try to use implicits to not use 'lex.keyword' everytime
-    // bool-liter := true | false
-    val boolLiter = lex.keyword("true") <|> lex.keyword("false")
+  // pair-liter := null
+  val pairLiter = "null"
 
-    // pair-liter := null
-    val pairLiter = "null"
+  // escaped-char := '0' | 'b' | 't' | 'n' | 'f' | 'r' | '"' | ''' | '\'
+  val escapedChar =
+    "0" <|> "b" <|> "t" <|> "n" <|> "f" <|> "r" <|> "\"" <|> "'" <|> "\\"
 
-    // escaped-char := '0' | 'b' | 't' | 'n' | 'f' | 'r' | '"' | ''' | '\'
-    val escapedChar = "0" <|> "b" <|> "t" <|> "n" <|> "f" <|> "r" <|> "\"" <|> "'" <|> "\\"
+  // TODO: fix behavior of char and string literals - escaped '\'' below not working
+  // character := (any ASCII char except '\', ''', '"') | <escaped-char>
+  val character = noneOf('"', '\'', '\\') <|> ("\\" <~> escapedChar)
 
-    // TODO: fix behavior of char and string literals - escaped '\'' below not working
-    // character := (any ASCII char except '\', ''', '"') | <escaped-char>
-    val character = noneOf('"', '\'', '\\') <|> "\\" <~> escapedChar
+  // char-liter := ''' <character> '''
+  val charLiter = "'" ~> character <~ "'"
 
-    // char-liter := ''' <character> '''
-    val charLiter = "'" ~> character <~ "'"
+  // string-liter := '"' <character>* '"'
+  val stringLiter = "\"" ~> many(character) <~ "\""
 
-    // string-liter := '"' <character>* '"'
-    val stringLiter = "\"" ~> many(character) <~ "\""
+  // ident := ('_' | 'a'-'z' | 'A'-'Z') ('_' | 'a'-'z' | 'A'-'Z' | '0'-'9')*
+  // val ident = ???
 
-    // ident := ('_' | 'a'-'z' | 'A'-'Z') ('_' | 'a'-'z' | 'A'-'Z' | '0'-'9')*
-    // val ident = ???
+  // TODO: remove - this is here for testing purposes
+  val literal =
+    intLiter <|> boolLiter <|> pairLiter <|> charLiter <|> stringLiter <|> lex
+      .keyword("comment")
 
-    // TODO: remove - this is here for testing purposes
-    val literal = intLiter <|> boolLiter <|> pairLiter <|> charLiter <|> stringLiter <|> lex.keyword("comment")
-
-
-    // TODO: change this at the end - currently set to check literals
-    // program := 'begin' <func>* <stat> 'end'
-    val program = fully(lex.keyword("begin") ~> literal <~ lex.keyword("end"))
+  // TODO: change this at the end - currently set to check literals
+  // program := 'begin' <func>* <stat> 'end'
+  val program = fully(lex.keyword("begin") ~> literal)
 }
--- src/main/scala/SyntaxParser.scala
+++ src/main/scala/SyntaxParser.scala
@@ -3,19 +3,19 @@ import scala.util.Try
 import parsley.io.{ParseFromIO}
 
 object SyntaxParser {
-    import parsley.{Success, Failure}
-    def main(args: Array[String]): Unit = {
+  import parsley.{Success, Failure}
+  def main(args: Array[String]): Unit = {
     assert(args.length == 1, "Usage: ./compile <wacc filename>")
     println("Parsing file: " + args(0))
     val waccFile = new File(args(0))
     val parseResult = syntax.program.parseFromFile(waccFile)
-         parseResult.get match {
-            case Success(x) =>
-                println(s"${args(0)} is valid!")
-                System.exit(0)
-            case Failure(err) =>
-                println(err)
-                System.exit(200)
-        }
+    parseResult.get match {
+      case Success(x) =>
+        println(s"${args(0)} is valid!")
+        System.exit(0)
+      case Failure(err) =>
+        println(err)
+        System.exit(200)
     }
+  }
 }
