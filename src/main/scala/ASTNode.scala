1e218518105b64f3a147c38da5d134bdf5f01543
Jacqueline Lee <jacqleegege@gmail.com>
2022-02-04 11:36:34 +0000

    Added Unary and Binary Operators

--- src/main/scala/ASTNode.scala
+++ src/main/scala/ASTNode.scala
@@ -36,11 +36,37 @@ sealed trait PairElemTypeNode extends ASTNode
 
 // Expression
 sealed trait ExprNode extends ASTNode
+case class 
+
 
 // Unary Operator
-sealed trait UnaryOpNode extends ASTNode
+sealed trait UnaryOpNode extends ExprNode
+case class Not(x: ExprNode) extends UnaryOpNode
+case class Neg(x: ExprNode) extends UnaryOpNode
+case class Len(x: ExprNode) extends UnaryOpNode
+case class Ord(x: ExprNode) extends UnaryOpNode
+case class Chr(x: ExprNode) extends UnaryOpNode
+
 
 // Binary Operator
-sealed trait BinaryOpNode extends ASTNode
+sealed trait BinaryOpNode extends ExprNode
+case class Mult(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Div(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Mod(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Add(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Sub(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Greater(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class GreaterOrEqual(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Smaller(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class SmallerOrEqual(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Equal(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class NotEqual(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Equal(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class NotEqual(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class And(x: ExprNode, y: ExprNode) extends BinaryOpNode
+case class Or(x: ExprNode, y: ExprNode) extends BinaryOpNode
+
+
+
 
 // Indentifier
