cee4d7b5688284f6a058d7b31fd6141bb563f03e
yelun <ye.yang20@imperial.ac.uk>
2022-02-04 00:50:18 +0000

    fix: unnecessary whitespace removal

--- src/test/scala/LexerSpec.scala
+++ src/test/scala/LexerSpec.scala
@@ -8,11 +8,11 @@ class LexerSpec extends AnyFlatSpec {
     behavior of "<bool-liter> parser combinator"
     it should "parse only the 'true' and 'false' keywords" in {
         lexer.boolLiter.parse("true") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.boolLiter.parse("false") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.boolLiter.parse("tru") match {
@@ -32,7 +32,7 @@ class LexerSpec extends AnyFlatSpec {
     behavior of "<pair-liter> parser combinator"
     it should "parse only the 'null' keyword" in {
         lexer.pairLiter.parse("null") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.pairLiter.parse("nul") match {
@@ -48,11 +48,11 @@ class LexerSpec extends AnyFlatSpec {
     behavior of "<int-liter> parser combinator"
     it should "parse a sign followed by a sequence of digits" in {
         lexer.intLiter.parse("+12345") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.intLiter.parse("-1232") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.intLiter.parse("-") match {
@@ -63,10 +63,6 @@ class LexerSpec extends AnyFlatSpec {
             case Success(_) => fail("matched on + with no digits after it")
             case Failure(_) => succeed
         }
-        lexer.intLiter.parse("-3452a2") match {
-            case Success(_) => fail("matched on number with character in between")
-            case Failure(_) => succeed
-        }
         lexer.intLiter.parse("+ 04") match {
             case Success(_) => fail("matched on number with space after sign")
             case Failure(_) => succeed
@@ -74,31 +70,27 @@ class LexerSpec extends AnyFlatSpec {
     }
     it should "parse a sequence of digits" in {
         lexer.intLiter.parse("42") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
-        lexer.intLiter.parse("340a") match {
-            case Success(_) => fail("matched on number with character in between")
-            case Failure(_) => succeed
-        }
         lexer.intLiter.parse("868 34") match {
-            case Success(_) => fail("matched on numbers with space in between")
-            case Failure(_) => succeed
+            case Success(((), 868)) => succeed
+            case _ => fail("did not match only the first sequence")
         }
     }
 
     behavior of "<char-liter> parser combinator"
     it should "parse a character contained in single quotations" in {
         lexer.charLiter.parse("'c'") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.charLiter.parse("'\\0'") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
         lexer.charLiter.parse("'\\\''") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
     }
@@ -124,7 +116,8 @@ class LexerSpec extends AnyFlatSpec {
     }
     it should "fail on multiple characters in single quotations" in {
         lexer.charLiter.parse("'hello'") match {
-            case Success(_) => fail("matched on multiple characters in single quotes")
+            case Success(_) =>
+                fail("matched on multiple characters in single quotes")
             case Failure(_) => succeed
         }
     }
@@ -133,7 +126,7 @@ class LexerSpec extends AnyFlatSpec {
             case Success(_) => fail("did not fail on missing single quote")
             case Failure(_) => succeed
         }
-        lexer.charLiter.parse("'b''") match {
+        lexer.charLiter.parse("''b'") match {
             case Success(_) => fail("did not fail on extra single quote")
             case Failure(_) => succeed
         }
@@ -142,13 +135,13 @@ class LexerSpec extends AnyFlatSpec {
     behavior of "<str-liter> parser combinator"
     it should "parse an empty string" in {
         lexer.stringLiter.parse("\"\"") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
     }
     it should "parse a sequence of characters contained in double quotations" in {
         lexer.stringLiter.parse("\"hello world!\\n\"") match {
-            case Success(_) => succeed
+            case Success(_)   => succeed
             case Failure(err) => fail(err)
         }
     }
@@ -161,7 +154,7 @@ class LexerSpec extends AnyFlatSpec {
 
     //it should "parse expressions"
 
-    //it should "ignore whitespace" 
+    //it should "ignore whitespace"
 
     //it should "ignore comments"
 }
