20d35d6891872cf92ce71fbd022e4e8a974a8a0d
Athena Yim <asy20@ic.ac.uk>
2022-02-06 02:05:03 +0000

    refactor: rename test files for clarity and abstract token matching code

---
+++ src/test/scala/FrontendSpec.scala
@@ -0,0 +1,34 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import parsley.Parsley, Parsley._
+import java.io.File
+
+object utils {
+    def waccProgramsInDir(dir: File): Array[File] = {
+        if (dir.exists && dir.isDirectory) {
+            val current = dir.listFiles
+            val currentWacc = current.filter(_.isFile).filter { 
+                file => file.getName.endsWith(".wacc")
+            }
+            
+            currentWacc ++ current.filter(_.isDirectory).flatMap(d => waccProgramsInDir(d))
+        } else {
+            Array[File]()
+        }
+    }
+}
+
+class FrontendSpec extends AnyFlatSpec {
+    // TODO: load programs in batches with more specific test messages
+    val valid = utils.waccProgramsInDir(new File("./programs/valid"))
+    val invalid = utils.waccProgramsInDir(new File("./programs/invalid/syntaxErr"))
+
+    behavior of "compiler front-end"
+    it should "get valid programs from folder" in {
+        assert(valid.nonEmpty)
+    }
+
+    it should "get invalid programs from folder" in {
+        assert(invalid.nonEmpty)
+    }
+}
