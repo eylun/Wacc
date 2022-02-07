b3cbbb506d2594e8f6421f8ffe4b05f0c42a316a
Athena Yim <asy20@ic.ac.uk>
2022-02-07 00:42:09 +0000

    feat: define identifier objects to represent types

--- src/main/scala/Identifier.scala
+++ src/main/scala/Identifier.scala
@@ -1,1 +1,21 @@
+/* Identifier Objects */
+
 sealed trait Identifier
+
+sealed trait Type extends Identifier
+case class Variable(t: Type) extends Identifier
+case class Param(t: Type) extends Identifier
+
+/* Basic types */
+case class IntType(min: Int, max: Int) extends Type
+case class BoolType() extends Type
+case class CharType() extends Type
+case class StringType() extends Type
+
+case class NestedPairType() extends Type
+
+case class ArrayType(elemType: Type, elements: Int) extends Type
+case class PairType(fstType: Type, sndType: Type) extends Type
+
+case class FunctionId(returnType: Type, params: Array[Param], 
+                      symbolTable: SymbolTable) extends Identifier
