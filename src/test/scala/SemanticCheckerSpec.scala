ec46c0b75c82838ea144393c847832166ca6e773
Athena Yim <asy20@ic.ac.uk>
2022-02-10 22:58:01 +0000

    test: abstract out test fields setting into function and define base-type node checking

--- src/test/scala/SemanticCheckerSpec.scala
+++ src/test/scala/SemanticCheckerSpec.scala
@@ -4,14 +4,32 @@ import scala.collection.mutable.ListBuffer
 import testUtils.{assertTypeIdEquals}
 
 class SemanticCheckerSpec extends AnyFlatSpec {
-  behavior of "<type> nodes"
-    it should "interpret the correct type associated with the node" in {
-        val st = SymbolTable()
-        val log = ListBuffer[WaccError]()
-        val node = IntTypeNode()((0,0))
+    var st: SymbolTable = SymbolTable()
+    var log: ListBuffer[WaccError] = ListBuffer()
+    var node: ASTNode = IntTypeNode()((0,0))
 
+    def resetNode(n: ASTNode): Unit = {
+        this.st = SymbolTable()
+        this.log = ListBuffer()
+        this.node = n
+    }
+
+    behavior of "<type> nodes"
+    it should "correctly interpret <base-type> node types" in {
+        resetNode(IntTypeNode()((0,0)))
         node.check(st, log)
-
         assertTypeIdEquals(Some(IntType()), node.typeId, ListBuffer(), log)
+
+        resetNode(BoolTypeNode()((0,0)))
+        node.check(st, log)
+        assertTypeIdEquals(Some(BoolType()), node.typeId, ListBuffer(), log)
+
+        resetNode(CharTypeNode()((0,0)))
+        node.check(st, log)
+        assertTypeIdEquals(Some(CharType()), node.typeId, ListBuffer(), log)
+        
+        resetNode(StringTypeNode()((0,0)))
+        node.check(st, log)
+        assertTypeIdEquals(Some(StringType()), node.typeId, ListBuffer(), log)
     }
 }
