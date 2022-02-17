0690fe6883e14e048580bd31b5091d5105b50cbe
Athena Yim <asy20@ic.ac.uk>
2022-02-17 14:57:45 +0000

    feat: subtraction code gen

--- src/main/scala/CodeGeneration/InstructionBuilders/Expressions.scala
+++ src/main/scala/CodeGeneration/InstructionBuilders/Expressions.scala
@@ -11,9 +11,18 @@ object transExpression {
                 List(PushInstr(Reg(0))) ++ 
                 transExpression(e2) ++ 
                 List(MoveInstr(Reg(1), RegOp(Reg(0))), 
-                    PopInstr(Reg(0)), AddInstr(Reg(0), Reg(0), RegOp(Reg(1))))  
+                    PopInstr(Reg(0)), 
+                    AddInstr(Reg(0), Reg(0), RegOp(Reg(1))))  
             } // TODO add logic for overflow handling
-            
+            case Sub(e1, e2) => {
+                transExpression(e1) ++
+                List(PushInstr(Reg(0))) ++
+                transExpression(e2) ++
+                List(MoveInstr(Reg(1), RegOp(Reg(0))),
+                    PopInstr(Reg(0)), 
+                    SubInstr(Reg(0), Reg(0), RegOp(Reg(1))))
+            } // TODO add logic for overflow handling
+
             case IntLiterNode(n) => List(LoadImmOffsetInstr(Reg(0), n))
             case CharLiterNode(c) => List(MoveInstr(Reg(0), ImmOffset(c))) 
             case BoolLiterNode(true) => List(MoveInstr(Reg(0), ImmOffset(1))) 
