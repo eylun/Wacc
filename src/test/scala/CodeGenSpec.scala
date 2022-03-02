1f91c732746028f86d55ed3434e21604a0dfe51c
Athena Yim <asy20@ic.ac.uk>
2022-03-02 00:28:43 +0000

    test: int, char, bool, pair literal code generation

--- src/test/scala/CodeGenSpec.scala
+++ src/test/scala/CodeGenSpec.scala
@@ -1,26 +1,55 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
-// case IntLiterNode(n) =>
-// 	collector.addStatement(List(LoadImmIntInstr(Reg(0), n)))
-// case CharLiterNode(c) =>
-// 	collector.addStatement(List(MoveInstr(Reg(0), ImmOffset(c))))
-// case BoolLiterNode(true) =>
-// 	collector.addStatement(List(MoveInstr(Reg(0), ImmOffset(1))))
-// case BoolLiterNode(false) =>
-// 	collector.addStatement(List(MoveInstr(Reg(0), ImmOffset(0))))
-class CodegenSpec extends AnyFlatSpec {
+class CodeGenSpec extends AnyFlatSpec {
     import parsley.{Success, Failure}
     import testUtils.{assertCodegenEquals}
-    val sf: StackFrame = StackFrame(SymbolTable())
+
+    var sf: StackFrame = StackFrame(SymbolTable())
+    implicit var wbuffer: WaccBuffer = new WaccBuffer
+    
+    def reset(): Unit = {
+        sf = StackFrame(SymbolTable())
+        wbuffer = new WaccBuffer
+    }
+
+    def testExpr(node: ExprNode, expected: List[Instruction]) = {
+        transExpression(node, sf)
+        assertCodegenEquals(expected, wbuffer.emit())
+    }
+
     // TODO: Change WaccBuffer to initialize the header stuff separately, then uncomment the test
-    // behavior of "translating expressions"
-    // it should "translate integer literals" in {
-    //     implicit val wbuffer: WaccBuffer = new WaccBuffer
-    //     transExpression(IntLiterNode(1)(0, 0), sf)
-    //     assertCodegenEquals(
-    //       List(LoadImmIntInstr(Reg(0), 1)),
-    //       wbuffer.emit()
-    //     )
-    // }
+    behavior of "expression code generation"
+    it should "translate integer literals" in {
+        reset()
+        testExpr(IntLiterNode(1)(0,0), List(LoadImmIntInstr(Reg(0), 1)))
+        reset()
+        testExpr(IntLiterNode(-23)(0,0), List(LoadImmIntInstr(Reg(0), -23)))
+    }
+    it should "translate character literals" in {
+        reset()
+        testExpr(
+            CharLiterNode('d')(0,0), 
+            List(MoveInstr(Reg(0), ImmOffset('d')))
+        )
+    }
+    it should "translate boolean literals" in {
+        reset()
+        testExpr(
+            BoolLiterNode(true)(0,0), 
+            List(MoveInstr(Reg(0), ImmOffset(1)))
+        )
+        reset()
+        testExpr(
+            BoolLiterNode(false)(0,0),
+            List(MoveInstr(Reg(0), ImmOffset(0)))
+        )
+    }
+    it should "translate pair literals" in {
+        reset()
+        testExpr(
+            new PairLiterNode()(0,0),
+            List(MoveInstr(Reg(0), ImmOffset(0)))
+        )
+    }
 }
