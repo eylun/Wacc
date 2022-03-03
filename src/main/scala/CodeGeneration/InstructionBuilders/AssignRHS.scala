0491075ae87958a17e44ac13c7c83077d42c22f4
Jacqueline Lee <jacqleegege@gmail.com>
2022-03-03 18:09:57 +0000

    test:start assignrhs code generation

--- src/main/scala/CodeGeneration/InstructionBuilders/AssignRHS.scala
+++ src/main/scala/CodeGeneration/InstructionBuilders/AssignRHS.scala
@@ -31,7 +31,7 @@ object transRHS {
                           )
                         )
                     }
-                    case _ =>
+                    case _ => 
                 }
                 collector.addStatement(
                   List(
@@ -139,22 +139,25 @@ object transRHS {
                 collector.addStatement(
                   List(
                     BranchLinkInstr("p_check_null_pointer")
-                  ) ++
-                      (e match {
-                          case FirstPairElemNode(f) => {
-                              transExpression(f, stackFrame)
-                              List(
-                                LoadInstr(r0, r0, ImmOffset(0))
-                              )
-                          }
-                          case SecondPairElemNode(s) => {
-                              transExpression(s, stackFrame)
-                              List(
-                                LoadInstr(r0, r0, ImmOffset(WORD_SIZE))
-                              )
-                          }
-                      })
+                  )
                 )
+                collector.addStatement(   
+                    (e match {
+                        case FirstPairElemNode(f) => {
+                            transExpression(f, stackFrame)
+                            List(
+                            LoadInstr(r0, r0, ImmOffset(0))
+                            )
+                        }
+                        case SecondPairElemNode(s) => {
+                            transExpression(s, stackFrame)
+                            List(
+                            LoadInstr(r0, r0, ImmOffset(WORD_SIZE))
+                            )
+                        }
+                    })
+                    )
+
                 collector.addStatement(
                   List(determineLoadInstr(e.typeId.get.getType(), r0, r0, 0))
                 )
