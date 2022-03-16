d2c0eff120be97d1c29da83e274854bf64085f15
Lee Wan Sze <leewansze27@gmail.com>
2022-03-16 11:39:34 +0000

    chore: refactor function to remove unnecessary load and store pairs

--- src/main/scala/CodeGeneration/Optimisation/PeepholeOptimisation.scala
+++ src/main/scala/CodeGeneration/Optimisation/PeepholeOptimisation.scala
@@ -19,73 +19,47 @@ object PeepholeOptimisation {
 
     def removeUnnecessaryLoadAndStore(i: Int)(implicit instrList: mutable.ListBuffer[Instruction]): Unit = {
         if (i < instrList.size - 4) {
-            (instrList(i), instrList(i + 1), instrList(i + 2), instrList(i + 3)) match {
-                case (
-                      LoadInstr(loadDst1, _, _, _),
-                      StoreInstr(storeSrc1, storeDst1, offset1, _),
-                      LoadInstr(loadDst2, _, _, _),
-                      StoreInstr(storeSrc2, storeDst2, offset2, _)
-                    )
-                    if ((loadDst1 == storeSrc1) == (loadDst2 == storeSrc2)) &&
-                        storeDst1 == storeDst2 && offset1 == offset2 => {
-                    instrList.remove(i)
-                    instrList.remove(i)
-                    removeUnnecessaryLoadAndStore(i)
+            (instrList(i), instrList(i + 1)) match {
+                case (LoadInstr(loadDst1, _, _, _), StoreInstr(storeSrc1, storeDst1, offset1, _)) => {
+                    (instrList(i + 2), instrList(i + 3)) match {
+                        case (LoadInstr(`loadDst1`, _, _, _), StoreInstr(`storeSrc1`, `storeDst1`, `offset1`, _)) |
+                            (LoadImmIntInstr(`loadDst1`, _, _), StoreInstr(`storeSrc1`, `storeDst1`, `offset1`, _)) => {
+                            instrList.remove(i)
+                            instrList.remove(i)
+                            removeUnnecessaryLoadAndStore(i)
+                        }
+                        case _ =>
+                    }
 
                 }
 
-                case (
-                      LoadInstr(loadDst1, _, _, _),
-                      StoreInstr(storeSrc1, storeDst1, offset1, _),
-                      LoadImmIntInstr(loadDst2, _, _),
-                      StoreInstr(storeSrc2, storeDst2, offset2, _)
-                    )
-                    if ((loadDst1 == storeSrc1) == (loadDst2 == storeSrc2)) &&
-                        storeDst1 == storeDst2 && offset1 == offset2 => {
-                    instrList.remove(i)
-                    instrList.remove(i)
-                    removeUnnecessaryLoadAndStore(i)
-                }
-
-                case (
-                      LoadImmIntInstr(loadDst1, _, _),
-                      StoreInstr(storeSrc1, storeDst1, offset1, _),
-                      LoadInstr(loadDst2, _, _, _),
-                      StoreInstr(storeSrc2, storeDst2, offset2, _)
-                    )
-                    if ((loadDst1 == storeSrc1) == (loadDst2 == storeSrc2)) &&
-                        storeDst1 == storeDst2 && offset1 == offset2 => {
-                    instrList.remove(i)
-                    instrList.remove(i)
-                    removeUnnecessaryLoadAndStore(i)
-                }
-
-                case (
-                      LoadImmIntInstr(loadDst1, _, _),
-                      StoreInstr(storeSrc1, storeDst1, offset1, _),
-                      LoadImmIntInstr(loadDst2, _, _),
-                      StoreInstr(storeSrc2, storeDst2, offset2, _)
-                    )
-                    if ((loadDst1 == storeSrc1) == (loadDst2 == storeSrc2)) &&
-                        storeDst1 == storeDst2 && offset1 == offset2 => {
-                    instrList.remove(i)
-                    instrList.remove(i)
-                    removeUnnecessaryLoadAndStore(i)
-                }
-
-                case (
-                      LoadRegSignedByte(loadDst1, _, _, _),
-                      StoreByteInstr(storeSrc1, storeDst1, offset1, _),
-                      LoadRegSignedByte(loadDst2, _, _, _),
-                      StoreByteInstr(storeSrc2, storeDst2, offset2, _)
-                    )
-                    if ((loadDst1 == storeSrc1) == (loadDst2 == storeSrc2)) &&
-                        storeDst1 == storeDst2 && offset1 == offset2 => {
-                    instrList.remove(i)
-                    instrList.remove(i)
-                    removeUnnecessaryLoadAndStore(i)
+                case (LoadImmIntInstr(loadDst1, _, _), StoreInstr(storeSrc1, storeDst1, offset1, _)) => {
+                    (instrList(i + 2), instrList(i + 3)) match {
+                        case (LoadInstr(`loadDst1`, _, _, _), StoreInstr(`storeSrc1`, `storeDst1`, `offset1`, _)) |
+                            (LoadImmIntInstr(`loadDst1`, _, _), StoreInstr(`storeSrc1`, `storeDst1`, `offset1`, _)) => {
+                            instrList.remove(i)
+                            instrList.remove(i)
+                            removeUnnecessaryLoadAndStore(i)
+                        }
+                        case _ =>
+                    }
 
                 }
+                case (LoadRegSignedByte(loadDst1, _, _, _), StoreByteInstr(storeSrc1, storeDst1, offset1, _)) => {
+                    (instrList(i + 2), instrList(i + 3)) match {
+                        case (
+                              LoadRegSignedByte(`loadDst1`, _, _, _),
+                              StoreByteInstr(`storeSrc1`, `storeDst1`, `offset1`, _)
+                            ) => {
+                            instrList.remove(i)
+                            instrList.remove(i)
+                            removeUnnecessaryLoadAndStore(i)
+                        }
+                        case _ =>
+                    }
+
+                }
+
                 case _ =>
             }
         }
